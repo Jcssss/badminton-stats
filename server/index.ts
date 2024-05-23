2bd410a23194fa0f99035a9631b034c805d55116
Justin Siu <justsiu545@gmail.com>
2024-05-22 23:38:12 -0700

    adding ability to get overall rank data for top X players over a certain time frame, currently a little buggy

--- server/index.ts
+++ server/index.ts
@@ -20,33 +20,48 @@ app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
-app.get("/api", async (req, res) => {
+app.get("/api/playerrank", async (req, res) => {
     const data = await getPlayerRankingData(
         req.query.event as string, 
         req.query.player as string,
         parseInt(req.query.weeks as string),
     );
-    //const data = await getPlayerData(req.query.player as string);
+    res.json({ message: data });
+});
+
+app.get("/api/playerbio", async (req, res) => {
+    const data = await getPlayerData(req.query.player as string);
+    res.json({ message: data });
+});
+
+app.get("/api/ranks", async (req, res) => {
+    const data = await getOverallRankingData(
+        req.query.event as string, 
+        parseInt(req.query.players as string),
+        parseInt(req.query.weeks as string),
+    );
     res.json({ message: data });
 });
 
 // Initializes a browser window for puppeteer
-const initBrowser = async () => {  
+const initPage = async (url: string): Promise<puppeteer.Page> => {  
     const browser = await puppeteer.launch({
-        headless: false,
+        //headless: false,
     });
-    return browser;
+
+    let page = await browser.newPage();
+    await page.goto(`${url}`, {
+        waitUntil: "domcontentloaded",
+    });
+    
+    return page;
 };
 
 // Given a player's name, searches for the player in the search bar and returns a matching result.
 const getPlayerData = async (player: string): Promise<string> => {
     
-    // Initializes browser and loads pages
-    const browser = await initBrowser();
-    let page = await browser.newPage();
-    await page.goto('https://bwf.tournamentsoftware.com/ranking/ranking.aspx?rid=70', {
-        waitUntil: "domcontentloaded",
-    });
+    // Initializes browser and loads page
+    let page = await initPage('https://bwf.tournamentsoftware.com/ranking/ranking.aspx?rid=70');
 
     await page.type('input.search-box__field', player);
     try {
@@ -60,38 +75,32 @@ const getPlayerData = async (player: string): Promise<string> => {
         playerLink.click();
     })
 
-    //await browser.close();
-    return '';
+    await page.browser().close();
+    return 'Got Bio';
 }
 
-// Gets the overall rankings in a specific event or of a specific player
+// Gets the overall rankings of a specific player
 const getPlayerRankingData = async (
     event: string, player: string, weeks: number
 ): Promise<[[string, string]]> => {
     
     // Initializes browser and loads pages
-    const browser = await initBrowser();
-    let page = await browser.newPage();
-    await page.goto(`${urls[event]}`, {
-        waitUntil: "domcontentloaded",
-    });
+    let page = await initPage(urls[event]);
 
     let rankingData: [[string, string]] = [['Rank', 'Points']];
     for (let i = 0; i < weeks; i++) {
         
         await selectWeek(page, i);
 
-        console.log(await page.url());
-        
         // Loops through all pages until reaches the last page or finds the player
-        let playerData: [string, string] = ['', '']
-        while (playerData[0] == '') {
+        let weekData: [string, string] = ['', '']
+        while (weekData[0] == '') {
 
             // Searches the current page for the player
-            playerData = await findPlayerPointRank(page, player);
+            weekData = await findPlayerPointRank(page, player);
 
             // If the player isn't on the page, goes to next page
-            if (playerData[0] == '') {
+            if (weekData[0] == '') {
                 let pageLink = await getNextPageLink(page);
 
                 // If there are no next pages, informs the user
@@ -106,15 +115,57 @@ const getPlayerRankingData = async (
                 }
             }
         }
-
-        rankingData.push(playerData)
+        rankingData.push(weekData)
     }
 
-    //await browser.close();
+    await page.browser().close();
     return rankingData;
 };
 
-// // Given a page, returns the url for the next page
+// Gets the overall rankings of the top X player
+const getOverallRankingData = async (
+    event: string, players: number, weeks: number
+): Promise<[[[string, string, string]]]> => {
+    
+    // Initializes browser and loads pages
+    let page = await initPage(urls[event]);
+
+    let rankingData: [[[string, string, string]]] = [[['Name', 'Rank', 'Points']]];
+    for (let i = 0; i < weeks; i++) {
+        
+        await selectWeek(page, i);
+
+        // Loops through all pages until reaches the last page or finds the player
+        let weekData: [[string, string, string]] = [['', '', '']]
+        while (weekData[0][0] == '') {
+
+            // Searches the current page for the player
+            weekData = await findWeeklyPointRank(page, players);
+
+            // If the player isn't on the page, goes to next page
+            if (weekData[0][0] == '') {
+                let pageLink = await getNextPageLink(page);
+
+                // If there are no next pages, informs the user
+                if (pageLink === '') {
+                    rankingData.push([['', '', '']]);
+                
+                // If there is another page, moves to that page
+                } else {
+                    await page.goto(`${pageLink}`, {
+                        waitUntil: "domcontentloaded",
+                    });
+                }
+            }
+        }
+        rankingData.push(weekData)
+    }
+
+    await page.browser().close();
+    return rankingData;
+};
+
+// Given a page, returns the url for the next page
 const getNextPageLink = async (page: puppeteer.Page):Promise<string> => {
     
     const nextPageLink = await page.evaluate((): string => {
@@ -129,11 +180,11 @@ const getNextPageLink = async (page: puppeteer.Page):Promise<string> => {
     return nextPageLink;
 }
 
-// // Given a page, searches the list of players for the specified player
+// Given a page, searches the list of players for the specified player and returns their points and rank
 const findPlayerPointRank = async (page: puppeteer.Page, player: string):Promise<[string, string]> => {
     
     // Extract text content of elements matching the selector
-    const pointRank = await page.evaluate((player: string): [string, string] => {
+    const pointRank = await page.evaluate((player): [string, string] => {
         const links = document.querySelectorAll('table.ruler > tbody > tr > td > a');
         const playerLink: HTMLElement = Array.from(links).find((link) => {
             if (link.textContent.toLowerCase() == player.toLowerCase()) {
@@ -156,17 +207,41 @@ const findPlayerPointRank = async (page: puppeteer.Page, player: string):Promise
     return pointRank;
 }
 
+// Given a page, searches the list of players for the specified player and returns their points and rank
+const findWeeklyPointRank = async (page: puppeteer.Page, players: number):Promise<[[string, string, string]]> => {
+    
+    let data: [[string, string, string]];
+
+    // Gets the number of players on the page, excludes the column headers,
+    // blank row, and page number footer
+    const playersOnPage = await page.evaluate(() => {
+        const playerContainer = document.querySelector('table.ruler > tbody');
+        return playerContainer.childElementCount - 3
+    })
+    
+    for (let playerNum = 0; playerNum < playersOnPage; playerNum++) {
+        let rankData = await page.evaluate((playerNum): [string, string, string] => {
+            let curPlayer = document.querySelector(`table.ruler > tbody > tr:nth-child(${playerNum + 2})`);
+            
+            if (curPlayer) {
+                let rank = curPlayer.querySelector('td.rank').textContent;
+                let points = curPlayer.querySelector('td.rankingpoints').textContent;
+                let name = curPlayer.querySelector('span.flag').textContent;
+                return [name, rank, points];
+            } else {
+                return ['', '', ''];
+            }
+
+        }, playerNum);
+
+        data.push(rankData);
+    }
+
+    return data;
+}
+
 const selectWeek = async (page: puppeteer.Page, week: number) => {
     await page.click('a.chosen-single');
-
-    // await page.evaluate((week: number) => {
-    //     let options = document.querySelectorAll('.active-result');
-    //     let anchor = Array.from(options).find((link): boolean => {
-    //         return week.toString() == link.getAttribute('data-option-array-index');
-    //     }) as HTMLElement;
-    //     anchor.click();
-    // }, week)
-
     await page.click(`.chosen-results > li:nth-child(${week + 1})`);
     await page.waitForSelector('a.chosen-single');
 }
